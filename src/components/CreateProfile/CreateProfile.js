b074d240fa2976515412d527e861c13c0119b111
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-11 17:36:58 +0100

    created a form and newuser state

--- src/components/CreateProfile/CreateProfile.js
+++ src/components/CreateProfile/CreateProfile.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CreateProfile = () => {
+const CreateProfile = ({ setUser }) => {
+  const [newUser, setNewUser] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
   return (
     <div>
       <h1>Create Profile</h1>
+      <form>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input name="name" id="name" type="text" onChange={handleChange} />
+        <br />
+        <label htmlFor="age">Age</label>
+        <br />
+        <input name="age" id="age" type="number" onChange={handleChange} />
+        <br />
+        <label htmlFor="location">Location</label>
+        <br />
+        <input
+          name="location"
+          id="location"
+          type="text"
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="bio">Bio</label>
+        <br />
+        <textarea name="bio" id="bio" type="text" onChange={handleChange} />
+      </form>
     </div>
   );
 };
