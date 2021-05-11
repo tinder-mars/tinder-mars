176dbc0c422addbdd61b22ea9ffa5444a5a00237
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-11 11:41:57 +0100

    added states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [favourites, setFavourites] = useState([]);
+  const [allRobots, setAllRobots] = useState([]);
+  const [currentRobot, setCurrentRobot] = useState(0);
+
   return <div className="App"></div>;
 }
 
