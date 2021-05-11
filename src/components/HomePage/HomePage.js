81fe5d925d63200f315b8ef84ca9b352b7c07547
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-11 23:55:54 +0100

    finished home-page-css

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
-import css from "./HomePage.css";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
