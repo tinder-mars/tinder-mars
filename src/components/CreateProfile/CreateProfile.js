35614792b6a138210ccc61de71cb1fcdbae856c8
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-12 01:01:04 +0100

    finished createprofile.css

--- src/components/CreateProfile/CreateProfile.js
+++ src/components/CreateProfile/CreateProfile.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import uploadimage from "../assets/uploadimage.svg";
+import "./CreateProfile.css";
 
 const CreateProfile = ({ setUser, history }) => {
   const defaultNewUser = {
@@ -26,18 +27,24 @@ const CreateProfile = ({ setUser, history }) => {
   };
 
   return (
-    <div>
-      <h1>Create Profile</h1>
+    <div className="create-profile-wrapper">
+      <h1 className="create-profile-title">Create Profile</h1>
       {isImageFormDisplayed ? (
         <div>
-          <label htmlFor="image">Upload Image</label>
+          <label htmlFor="image" className="create-profile-label">
+            Upload Image
+          </label>
           <input
             name="profile_image"
             id="profile_image"
             type="text"
             onChange={handleChange}
+            className="create-profile-input"
           />
-          <button onClick={() => setIsImageFormDisplayed(false)}>
+          <button
+            onClick={() => setIsImageFormDisplayed(false)}
+            className="create-profile-button"
+          >
             Confirm
           </button>
         </div>
@@ -47,31 +54,61 @@ const CreateProfile = ({ setUser, history }) => {
           alt="image"
           onClick={() => setIsImageFormDisplayed(true)}
           height="80"
+          className="create-profile-image"
         />
       )}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
+
+      <form onSubmit={handleSubmit} className="create-profile-form">
+        <label htmlFor="name" className="create-profile-label">
+          Name
+        </label>
         <br />
-        <input name="name" id="name" type="text" onChange={handleChange} />
+        <input
+          name="name"
+          id="name"
+          type="text"
+          onChange={handleChange}
+          className="create-profile-input"
+        />
         <br />
-        <label htmlFor="age">Age</label>
+        <label htmlFor="age" className="create-profile-label">
+          Age
+        </label>
         <br />
-        <input name="age" id="age" type="number" onChange={handleChange} />
+        <input
+          name="age"
+          id="age"
+          type="number"
+          onChange={handleChange}
+          className="create-profile-input"
+        />
         <br />
-        <label htmlFor="location">Location</label>
+        <label htmlFor="location" className="create-profile-label">
+          Location
+        </label>
         <br />
         <input
           name="location"
           id="location"
           type="text"
           onChange={handleChange}
+          className="create-profile-input"
         />
         <br />
-        <label htmlFor="bio">Bio</label>
+        <label htmlFor="bio" className="create-profile-label">
+          Bio
+        </label>
         <br />
-        <textarea name="bio" id="bio" onChange={handleChange} />
+        <textarea
+          name="bio"
+          id="bio"
+          onChange={handleChange}
+          className="create-profile-textarea"
+        />
         <br />
-        <button type="submit">Create User</button>
+        <button type="submit" className="create-profile-button">
+          Sign up
+        </button>
       </form>
     </div>
   );
