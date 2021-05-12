e38704dd0ebfbcb863b3a2240470cabc62f45848
Filipe Pires <flippires@gmail.com>
2021-05-12 01:14:26 +0100

    added matchlist css

--- src/components/MatchList/MatchList.js
+++ src/components/MatchList/MatchList.js
@@ -4,26 +4,35 @@
 
 import { Link } from "react-router-dom";
 import lonely_robot from "../assets/lonely_robot.svg";
+import "./MatchList.css";
+import matchlist_image from "../assets/matchlist.svg";
 
 const MatchList = ({ favourites }) => {
   return (
     <div>
-      {favourites.length ? (
-        favourites.map((item) => (
-          <div key={item.id}>
-            <Link to={`/chat/${item.id}`}>
-              <img src={item.profile_image} alt={item.name} height="40" />
-              <h2>{item.name}</h2>
-            </Link>
+      <img src={matchlist_image} className='matchlist_image'/>
+      <div className="list_container">
+        {favourites.length ? (
+          favourites.map((item) => (
+            <div key={item.id}>
+              <Link to={`/chat/${item.id}`} className="favourite_container">
+                <img
+                  className="avatar"
+                  src={item.profile_image}
+                  alt={item.name}
+                />
+                <h2 className="avatar_name">{item.name}</h2>
+              </Link>
+            </div>
+          ))
+        ) : (
+          <div>
+            <h1 className='No_Match_message'>You have no matches yet!</h1>
+            <br />
+            <img src={lonely_robot} alt="lonely_robot" className='lonely_robot'/>
           </div>
-        ))
-      ) : (
-        <div>
-          <h1>You have no matches yet!</h1>
-          <br />
-          <img src={lonely_robot} alt="lonely_robot" height="80" />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
